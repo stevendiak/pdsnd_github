f9ec509a27e144cd762e5afd50543a6d216f51a0
Steven Diakoumis <steven.diak@outlook.com>
2024-05-21 22:37:28 +1000

    Added description of bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#File provides information on bikeshare data in major US cities
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
